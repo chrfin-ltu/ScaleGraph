72a5dc065c05b04a0f9c267676dfc3f217e7f34e
Christoffer Fink <christoffer.fink@ltu.se>
2025-10-10 11:00:38 +0200

    Improved UDP
    - Can connect to a (probably named) process on start.
    - Ignores a (presumably temporarily) dead owner process. (Log error
      and continue.)
    - Improved log messages.
    - Support via-tuples for connected (owner) process.

--- apps/netsim/lib/netsim/udp.ex
+++ apps/netsim/lib/netsim/udp.ex
@@ -6,17 +6,25 @@ defmodule Netsim.UDP do
   Start UDP network server as a linked process.
   """
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, nil, opts)
+    GenServer.start_link(__MODULE__, opts, opts)
   end
 
   @doc """
   Opens a socket and registers the process for receiving messages from the
   network. The `address` is an `{ip, port}` pair.
+
+  If already connected, the socket is not opened, but the process is
+  "connected", which allows a different process to be connected to an existing
+  socket.
+
+  The process can be a PID, an atom (naming a registered process), or a via
+  tuple of the form `{:via, registry_module, {registry_process, key}}`, where
+  `key` is an arbitrary term.
   """
   def connect(net, address, process \\ self())
 
-  def connect(net, {_ip, _port} = dst, process) do
-    GenServer.call(net, {:connect, dst, process})
+  def connect(net, {_ip, _port} = bind_addr, process) do
+    GenServer.call(net, {:connect, bind_addr, process})
   end
 
   @doc """
@@ -27,16 +35,31 @@ defmodule Netsim.UDP do
   end
 
   @impl GenServer
-  def init(_opts) do
+  def init(opts) do
     state = %{socket: nil, owner: nil}
-    {:ok, state}
+    case Keyword.get(opts, :connect) do
+      nil ->
+        # :connect is optional
+        # TODO: might want to distinguish missing from explicitly `nil`
+        {:ok, state}
+
+      {{ip, port}, owner} ->
+        Logger.info("UDP.init: connecting")
+        state = _connect(ip, port, owner, state)
+        {:ok, state}
+
+      bad ->
+        Logger.error("UDP.init: bad connect: #{inspect(bad)}")
+        {:stop, "bad connect param"}
+    end
   end
 
   @impl GenServer
   def handle_call({:connect, {ip, port}, process}, _caller, state) do
-    if state.owner || state.socket do
-      Logger.error("already connected to UDP network, ignoring")
-      {:reply, :ignored, state}
+    state = Map.put(state, :owner, process)
+    if state.socket do
+      Logger.warning("UDP.connect: already connected, ignoring")
+      {:reply, :ok, state}
     else
       state = _connect(ip, port, process, state)
       {:reply, :ok, state}
@@ -46,14 +69,15 @@ defmodule Netsim.UDP do
   @impl GenServer
   def handle_cast({:send, dst, payload}, state) do
     if state.socket == nil do
-      Logger.error("no socket! Forgot to connect?")
+      Logger.error("UDP.send: no socket! Forgot to connect?")
     end
-
+    # Let it crash!
     :ok = :gen_udp.send(state.socket, dst, payload)
     {:noreply, state}
   end
 
   defp _connect(ip, port, process, state) do
+    # TODO: make e.g. {:error :eaddrinuse} more explicit?
     {:ok, socket} = :gen_udp.open(port, ip: ip, active: true, mode: :binary)
     %{state | owner: process, socket: socket}
   end
@@ -65,9 +89,18 @@ defmodule Netsim.UDP do
   #   the UDP/IP header.
   # - Otherwise, if we keep the redundancy, it might make sense to check the
   #   two addresses to ensure they match.
+  #
+  # The owner process may be temporarily down when we try to deliver a message.
+  # For now, we log an error and ignore it (drop the message). Might try to do
+  # something more intelligent in the future.
   @impl GenServer
   def handle_info({:udp, _socket, _src_ip, _src_port, payload}, state) do
-    Kernel.send(state.owner, {:network, payload})
+    pid = GenServer.whereis(state.owner)
+    try do
+      Kernel.send(pid, {:network, payload})
+    rescue
+      e -> Logger.error("UDP: owner temporarily down? #{inspect(e)}")
+    end
     {:noreply, state}
   end
 end
---
+++ apps/netsim/test/netsim/udp_test.exs
@@ -0,0 +1,37 @@
+defmodule Netsim.UDPTest do
+  use ExUnit.Case
+  alias Netsim.UDP
+
+  # Since the tests open real ports, pick a "random" address and port that is
+  # unlikely to be in use.
+  @bind_port 24680
+  @bind_ip {127, 159, 264, 123}
+  @bind_addr {@bind_ip, @bind_port}
+
+  test "start, connect, send to self" do
+    {:ok, udp} = UDP.start_link([])
+    UDP.connect(udp, @bind_addr)
+    # Sending to self
+    UDP.send(udp, @bind_addr, "hello")
+    assert_receive {:network, "hello"}
+  end
+
+  test "with registry and via tuple" do
+    Registry.start_link(keys: :unique, name: __MODULE__)
+    key = {@bind_addr, :owner}
+    owner_via = {:via, Registry, {__MODULE__, key}}
+    {:ok, udp} = UDP.start_link([])
+    this = self()
+
+    owner_pid = spawn(fn ->
+      Registry.register(__MODULE__, key, nil)
+      UDP.connect(udp, @bind_addr, owner_via)
+      UDP.send(udp, @bind_addr, "hello")
+      assert_receive {:network, "hello"}
+      send(this, "done")
+    end)
+
+    assert_receive "done"
+  end
+
+end

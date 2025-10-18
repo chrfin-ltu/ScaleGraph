383ee65a76af03a35e0b5decddc64e4cc503e2bf
Christoffer Fink <christoffer.fink@ltu.se>
2025-10-18 17:28:15 +0200

    Refactor

--- apps/scalegraph/test/scale_graph/rpc_test.exs
+++ apps/scalegraph/test/scale_graph/rpc_test.exs
@@ -11,11 +11,13 @@ defmodule ScaleGraph.RPCTest do
       {:ok, net} = Fake.start_link([])
       addr1 = {{127, 0, 0, 1}, 12345}
       addr2 = {{127, 0, 0, 2}, 23456}
+      id1 = 123
+      id2 = 234
 
       {:ok, rpc1} =
         RPC.start_link(
           addr: addr1,
-          id: 123,
+          id: id1,
           net: {Fake, net},
           handler: self()
         )
@@ -23,26 +25,27 @@ defmodule ScaleGraph.RPCTest do
       {:ok, rpc2} =
         RPC.start_link(
           addr: addr2,
-          id: 234,
+          id: id2,
           net: {Fake, net},
           handler: self()
         )
 
-      %{id1: 123, id2: 234, addr1: addr1, addr2: addr2, rpc1: rpc1, rpc2: rpc2}
+      %{
+        id1: id1, id2: id2,
+        addr1: addr1, addr2: addr2,
+        src: {id1, addr1}, dst: {id2, addr2},
+        rpc1: rpc1, rpc2: rpc2
+      }
     end
 
 
     test "send ping", context do
-      %{id1: id1, id2: id2} = context
-      %{addr1: addr1, addr2: addr2, rpc1: rpc1, rpc2: rpc2} = context
-      src = {id1, addr1}
-      dst = {id2, addr2}
+      %{src: src, dst: dst, rpc1: rpc1, rpc2: rpc2} = context
       RPC.ping(rpc1, dst)
       assert_receive {:rpc_request, {:ping, {^src, ^dst, nil, _id}}} = req
       assert RPC.src(req) == src
       assert RPC.dst(req) == dst
-      src = {id2, addr2}
-      dst = {id1, addr1}
+      {src, dst} = {dst, src}
       RPC.ping(rpc2, dst)
       assert_receive {:rpc_request, {:ping, {^src, ^dst, nil, _id}}} = req
       assert RPC.src(req) == src
@@ -51,10 +54,7 @@ defmodule ScaleGraph.RPCTest do
 
 
     test "ping pong", context do
-      %{id1: id1, id2: id2} = context
-      %{addr1: addr1, addr2: addr2, rpc1: rpc1, rpc2: rpc2} = context
-      src = {id1, addr1}
-      dst = {id2, addr2}
+      %{src: src, dst: dst, rpc1: rpc1, rpc2: rpc2} = context
       RPC.ping(rpc1, dst)
       assert_receive {:rpc_request, {:ping, {^src, ^dst, nil, id}}} = req
       RPC.respond(rpc2, req, nil)
@@ -65,11 +65,8 @@ defmodule ScaleGraph.RPCTest do
     # The response to a request is delivered to the sender (not necessarily the
     # handler process).
     test "response is delivered to caller", context do
-      %{id1: id1, id2: id2} = context
-      %{addr1: addr1, addr2: addr2, rpc1: rpc1, rpc2: rpc2} = context
+      %{src: src, dst: dst, rpc1: rpc1, rpc2: rpc2} = context
       parent = self()
-      src = {id1, addr1}
-      dst = {id2, addr2}
       spawn(fn ->
         RPC.ping(rpc1, dst)
         assert_receive {:rpc_response, {:ping, {^dst, ^src, nil, _id}}}
@@ -84,11 +81,8 @@ defmodule ScaleGraph.RPCTest do
     # Responses can also be delivered to a designated receiver instead of
     # the sender.
     test "response delivered to specified process", context do
-      %{id1: id1, id2: id2} = context
-      %{addr1: addr1, addr2: addr2, rpc1: rpc1, rpc2: rpc2} = context
+      %{src: src, dst: dst, rpc1: rpc1, rpc2: rpc2} = context
       parent = self()
-      src = {id1, addr1}
-      dst = {id2, addr2}
       receiver = spawn(fn ->
         assert_receive {:rpc_response, {:ping, {^dst, ^src, nil, _id}}}
         send(parent, "done")
@@ -102,10 +96,7 @@ defmodule ScaleGraph.RPCTest do
 
     test "unexpected response is logged but not delivered to handler", context do
       import ExUnit.CaptureLog
-      %{id1: id1, id2: id2} = context
-      %{addr1: addr1, addr2: addr2, rpc1: rpc1} = context
-      src = {id1, addr1}
-      dst = {id2, addr2}
+      %{src: src, dst: dst, rpc1: rpc1} = context
       id = 12321
       req = {:rpc_request, {:ping, {src, dst, nil, id}}}
       assert capture_log(fn ->
@@ -116,22 +107,16 @@ defmodule ScaleGraph.RPCTest do
 
 
     test "find nodes request and response", context do
-      %{id1: id1, id2: id2} = context
-      %{addr1: addr1, addr2: addr2, rpc1: rpc1, rpc2: rpc2} = context
-      src = {id1, addr1}
-      dst = {id2, addr2}
+      %{src: src, dst: dst, rpc1: rpc1, rpc2: rpc2} = context
       RPC.find_nodes(rpc1, dst, 54321)
       assert_receive {:rpc_request, {:find_nodes, {^src, ^dst, 54321, id}}} = req
-      closest = [{234, addr2}]
+      closest = [dst]
       RPC.respond(rpc2, req, closest)
       assert_receive {:rpc_response, {:find_nodes, {^dst, ^src, ^closest, ^id}}}
     end
 
     test "ping with timeout", context do
-      %{id1: id1, id2: id2} = context
-      %{addr1: addr1, addr2: addr2, rpc1: rpc1} = context
-      src = {id1, addr1}
-      dst = {id2, addr2}
+      %{src: src, dst: dst, rpc1: rpc1} = context
       RPC.ping(rpc1, dst, timeout: 50)
       assert_receive {:rpc_request, {:ping, {^src, ^dst, nil, _id}}} = req
       # Don't respond. Eventually we get a timeout.
@@ -140,10 +125,7 @@ defmodule ScaleGraph.RPCTest do
 
     test "ping with timeout and late response", context do
       import ExUnit.CaptureLog
-      %{id1: id1, id2: id2} = context
-      %{addr1: addr1, addr2: addr2, rpc1: rpc1, rpc2: rpc2} = context
-      src = {id1, addr1}
-      dst = {id2, addr2}
+      %{src: src, dst: dst, rpc1: rpc1, rpc2: rpc2} = context
       RPC.ping(rpc1, dst, timeout: 50)
       assert_receive {:rpc_request, {:ping, {^src, ^dst, nil, _id}}} = req
       # Don't respond. Eventually we get a timeout.
